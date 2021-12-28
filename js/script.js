3020b97f68c95c8952bab7ba489b83dfcf5735f7
huynhnam1711 <namhp1711@gmail.com>
2021-12-28 19:46:55 +0700

    add file

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ function hidden() {
 
 button.addEventListener('click', show)
 closeBtn.addEventListener('click', hidden)
+
+
+// day and night
+
+let themeBtn = document.querySelector('#theme-btn');
+
+themeBtn.onclick = () => {
+    themeBtn.classList.toggle('fa-sun');
+
+    if(themeBtn.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}
